787dd0c85e0faca6e48e19b0e5f794bb507bc961
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-22 00:58:56 +1100

    Update Weather Observation Station 6.sql

--- Hackerrank/1. SELECT query/Weather Observation Station 6.sql
+++ Hackerrank/1. SELECT query/Weather Observation Station 6.sql
@@ -2,7 +2,7 @@
 # Difficulty: Easy
 # Score: 10
 
-```MySQL
+- MySQL
 SELECT DISTINCT CITY FROM STATION
 WHERE (CITY LIKE 'a%') OR (CITY LIKE 'e%') OR (CITY LIKE 'i%') OR (CITY LIKE 'o%') OR (CITY LIKE 'u%')
-```
+
