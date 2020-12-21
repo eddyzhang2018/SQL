566153524f9717ea9f46be4c61d6787001c9bda0
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-21 23:56:20 +1100

    Update Weather Observation Station 5.sql

--- Hackerrank/1. SELECT query/Weather Observation Station 5.sql
+++ Hackerrank/1. SELECT query/Weather Observation Station 5.sql
@@ -2,3 +2,9 @@ Problem Statement: https://www.hackerrank.com/challenges/weather-observation-sta
 Difficulty: Easy
 Score: 30
 
+```SQL
+SELECT CITY, LENGTH(CITY) FROM STATION
+ORDER BY LENGTH(CITY), CITY LIMIT 1;
+SELECT CITY, LENGTH(CITY) FROM STATION
+ORDER BY LENGTH(CITY) DESC, CITY ASC LIMIT 1;
+```
