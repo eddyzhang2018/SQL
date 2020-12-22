4df693490843e26db80618e3c4477671a3516a02
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-22 13:06:59 +1100

    Update Weather Observation Station 8.sql

--- Hackerrank/1. SELECT query/Weather Observation Station 8.sql
+++ Hackerrank/1. SELECT query/Weather Observation Station 8.sql
@@ -22,9 +22,6 @@ WHERE
 - MySQL query using LIKE and IN operator AND LEFT() and RIGHT() fucntion
 # reference: Rex Liu, https://www.hackerrank.com/challenges/weather-observation-station-8/forum
 
-WHERE LEFT(CITY,1) in ('a','e','i','o','u')
-    AND RIGHT(CITY,1) in ('a','e','i','o','u')
-
 SELECT DISTINCT CITY FROM STATION
 WHERE LEFT(CITY,1) in ('a','e','i','o','u')
     AND RIGHT(CITY,1) in ('a','e','i','o','u')
