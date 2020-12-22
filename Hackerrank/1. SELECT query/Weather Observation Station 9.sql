62b4d3a31c94a9f4271b5fecc1fd6cafcd39b460
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-23 03:31:51 +1100

    Update Weather Observation Station 9.sql

--- Hackerrank/1. SELECT query/Weather Observation Station 9.sql
+++ Hackerrank/1. SELECT query/Weather Observation Station 9.sql
@@ -3,5 +3,12 @@
 # Score: 10
 
 - MySQL query:
+
 SELECT DISTINCT CITY FROM STATION
 WHERE LEFT(CITY, 1) NOT IN ('a','e','i','o','u')
+
+- MySQL query with REGULAR EXPRESSION:
+# Reference: Rodney Shaghoulian, https://www.hackerrank.com/challenges/weather-observation-station-9/forum
+
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[^aeiou]';
