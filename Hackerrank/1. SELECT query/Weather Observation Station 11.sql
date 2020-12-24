b83708e209b28b6376f96fb2933164bf466b4adf
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-25 01:19:20 +1100

    Update Weather Observation Station 11.sql

--- Hackerrank/1. SELECT query/Weather Observation Station 11.sql
+++ Hackerrank/1. SELECT query/Weather Observation Station 11.sql
@@ -1,3 +1,21 @@
 # Problem Statement: https://www.hackerrank.com/challenges/weather-observation-station-11/problem
 # Difficulty: Easy
 # Score: 15
+
+- MySQL using REGULAR EXPRESSION v1:
+
+SELECT DISTINCT CITY FROM STATION
+WHERE (CITY REGEXP '^[^aeiou]') OR (CITY REGEXP '[^aeiou]$')
+
+- MySQL using REGULAR EXPRESSION v2:
+
+SELECT DISTINCT CITY FROM STATION
+WHERE (CITY NOT REGEXP '^[aeiou]') OR (CITY NOT REGEXP '[aeiou]$')
+
+- MySQL using FUNCTION:
+
+SELECT DISTINCT CITY FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('a','e','i','o','u') OR
+      RIGHT(CITY, 1) NOT IN ('a','e','i','o','u')
+    
+- MySQL 
