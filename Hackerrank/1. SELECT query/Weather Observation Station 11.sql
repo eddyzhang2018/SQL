2b6f477bc23c4bee11dbcbff37c63e91c5e83021
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-25 01:34:31 +1100

    Update Weather Observation Station 11.sql

--- Hackerrank/1. SELECT query/Weather Observation Station 11.sql
+++ Hackerrank/1. SELECT query/Weather Observation Station 11.sql
@@ -17,5 +17,4 @@ WHERE (CITY NOT REGEXP '^[aeiou]') OR (CITY NOT REGEXP '[aeiou]$')
 SELECT DISTINCT CITY FROM STATION
 WHERE LEFT(CITY,1) NOT IN ('a','e','i','o','u') OR
       RIGHT(CITY, 1) NOT IN ('a','e','i','o','u')
-    
-- MySQL 
+
