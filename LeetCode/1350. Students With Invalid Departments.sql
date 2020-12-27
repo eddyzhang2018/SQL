78d66d71224e22c354e8a762185b059e1a489ed8
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-28 01:36:26 +1100

    Update 1350. Students With Invalid Departments.sql

--- LeetCode/1350. Students With Invalid Departments.sql
+++ LeetCode/1350. Students With Invalid Departments.sql
@@ -5,3 +5,8 @@
 - MySQL query:
 SELECT id, name from (SELECT * from Students WHERE department_id NOT IN (SELECT id from Departments)) as id_table
 
+- MySQL query v2:
+# Reference: MrDelhi, https://leetcode.com/problems/students-with-invalid-departments/discuss/508256/Simple-SQL-beats-100
+
+SELECT id, name FROM Students
+WHERE department_id not in (SELECT id from Departments)
