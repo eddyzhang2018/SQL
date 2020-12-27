cc08b0fca2ef65d9af940489a177fcc19facba95
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-28 00:18:07 +1100

    Update 1350. Students With Invalid Departments.sql

--- LeetCode/1350. Students With Invalid Departments.sql
+++ LeetCode/1350. Students With Invalid Departments.sql
@@ -1,5 +1,6 @@
 # Task: Write an SQL query to find the id and the name of all students who are enrolled in departments that no longer exists.
 # Problem Statements: https://leetcode.com/problems/students-with-invalid-departments/
+# Difficulty: Easy
 
 - MySQL query:
 SELECT id, name from (SELECT * from Students WHERE department_id NOT IN (SELECT id from Departments)) as id_table
