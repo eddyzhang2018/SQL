374052eaf6e6ddeeede4ac61a29a1ebad452e77b
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-28 00:36:18 +1100

    Create Employee Names.sql

---
+++ Hackerrank/1. SELECT query/Employee Names.sql
@@ -0,0 +1,8 @@
+# Problem Statement: https://www.hackerrank.com/challenges/name-of-employees/problem
+# Difficulty: Easy
+# Score: 10
+
+- MySQL query
+
+SELECT name from Employee
+ORDER BY name ASC
