1f1700691733121a9f2893493de48c624df63e3e
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-30 01:26:12 +1100

    Update 176. Second Highest Salary.sql

--- LeetCode/176. Second Highest Salary.sql
+++ LeetCode/176. Second Highest Salary.sql
@@ -6,3 +6,10 @@
 
 SELECT MAX(Salary) as SecondHighestSalary FROM Employee
 WHERE Salary < (SELECT MAX(Salary) FROM Employee);
+
+- MySQL query - leetcode solution:
+# reference: https://leetcode.com/problems/second-highest-salary/discuss/52952/A-Simple-Answer
+
+select (
+  select distinct Salary from Employee order by Salary Desc limit 1 offset 1
+)as SecondHighestSalary
