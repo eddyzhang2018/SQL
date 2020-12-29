e4d3ed0d9ea87c31addfafee6e65e4fa201407a4
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-29 15:53:51 +1100

    Update The PADS.sql

--- Hackerrank/2. Advanced Select/The PADS.sql
+++ Hackerrank/2. Advanced Select/The PADS.sql
@@ -4,3 +4,9 @@
 
 - MySQL query:
 
+SELECT CONCAT(Name,"(", LEFT(Occupation,1), ")") as Opt1 FROM OCCUPATIONS
+ORDER BY Name;
+SELECT CONCAT("There are a total of", " ", imm1, " ", LCASE(imm2), "s.") FROM 
+(SELECT COUNT(Occupation) as imm1, Occupation as imm2 FROM OCCUPATIONS
+ GROUP By Occupation
+ Order By imm1, imm2) as intermediate_table;
