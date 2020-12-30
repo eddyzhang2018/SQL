56832b7aaf8f071132f025ed8b861645686d3e4d
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2020-12-31 01:47:03 +1100

    Create 1179. Reformat Department Table.sql

---
+++ LeetCode/1179. Reformat Department Table.sql
@@ -0,0 +1,26 @@
+# Task: Write an SQL query to reformat the table such that there is a department id column and a revenue column for each month.
+# Problem Statement: https://leetcode.com/problems/reformat-department-table/
+# Difficulty: Easy
+
+- MS SQL Server (as use of PIVOT):
+
+SELECT id,
+    Jan AS Jan_Revenue,
+    Feb AS Feb_Revenue, 
+    Mar AS Mar_Revenue, 
+    Apr AS Apr_Revenue,
+    May AS May_Revenue,
+    Jun AS Jun_Revenue,
+    Jul AS Jul_Revenue,
+    Aug AS Aug_Revenue,
+    Sep AS Sep_Revenue,
+    Oct AS Oct_Revenue,
+    Nov AS Nov_Revenue,
+    Dec AS Dec_Revenue
+FROM Department
+PIVOT
+(SUM(revenue)
+ FOR month in (Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec)
+ ) AS PVT
+Order by id ASC;
+
