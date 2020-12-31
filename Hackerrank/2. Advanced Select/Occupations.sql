7eb3daea2710d8aad9694efa33c794e9fb61853f
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-01-01 02:52:04 +1100

    Update Occupations.sql

--- Hackerrank/2. Advanced Select/Occupations.sql
+++ Hackerrank/2. Advanced Select/Occupations.sql
@@ -33,3 +33,26 @@ FROM
         ORDER BY NAME) as rnk
 FROM OCCUPATIONS as o1) as o2
 GROUP BY o2.rnk
+
+- MySQL Queyr version 2 (with similar idea above but using ITERATIONS):
+# Reference 1: Yuji Shen (@YujiShen), https://www.hackerrank.com/challenges/occupations/forum/comments/114779
+# Reference 2: Yanger Rai (@raiyanger24), https://www.hackerrank.com/challenges/occupations/forum/comments/731399
+
+SET @doctor=0, @professor=0, @singer=0, @actor=0;
+SELECT MAX(Doctor), MAX(Professor), MAX(Singer), MAX(Actor)
+FROM
+(SELECT
+    CASE
+    WHEN Occupation='Doctor' THEN (@doctor:=@doctor+1)
+    WHEN Occupation='Professor' THEN (@professor:=@professor+1)
+    WHEN Occupation='Singer' THEN (@singer:=@singer+1)
+    WHEN Occupation='Actor' THEN (@actor:=@actor+1)
+    END AS nbrow, 
+    CASE WHEN Occupation='Doctor' THEN Name ELSE NULL END AS Doctor,
+    CASE WHEN Occupation='Professor' THEN Name ELSE NULL END AS Professor,
+    CASE WHEN Occupation='Singer' THEN Name ELSE NULL END AS Singer,
+    CASE WHEN Occupation='Actor' THEN Name ELSE NULL END AS Actor
+FROM OCCUPATIONS
+ORDER BY Name) AS temptbl
+GROUP BY nbrow;
+      
