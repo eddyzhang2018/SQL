425bddbff9ba5836d6d2bf01efff76d569a1f085
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-01-01 02:13:30 +1100

    Update Occupations.sql

--- Hackerrank/2. Advanced Select/Occupations.sql
+++ Hackerrank/2. Advanced Select/Occupations.sql
@@ -13,3 +13,23 @@ FROM
 (SELECT ROW_NUMBER() OVER(PARTITION BY Occupation ORDER BY NAME) AS RowNumber, * FROM OCCUPATIONS) AS tbl_1
 PIVOT
 (MIN(Name) FOR Occupation in (Doctor, Professor, Singer, Actor)) AS PVT;
+
+
+- MySQL Query using DENSE_RANK():
+# To work out this solution, I have a reference from Nobuhiro Hatano (@nobuh) 
+# Link: https://www.hackerrank.com/challenges/occupations/forum/comments/285802.
+# In his solution, he wrote a function to assign dense rank to each Name by Occupation
+# However, with DENSE_RANK() in MySQL. We can assign the number directly.
+                              
+SELECT
+MAX(CASE WHEN Occupation = 'Doctor' THEN Name ELSE NULL END) AS Doctor,
+MAX(CASE WHEN Occupation = 'Professor' THEN Name ELSE NULL END) AS Professor,
+MAX(CASE WHEN Occupation = 'Singer' THEN Name ELSE NULL END) AS Singer,
+MAX(CASE WHEN Occupation = 'Actor' THEN Name ELSE NULL END) AS Actor
+FROM
+(SELECT Name, Occupation, 
+        DENSE_RANK() OVER(
+        PARTITION BY Occupation
+        ORDER BY NAME) as rnk
+FROM OCCUPATIONS as o1) as o2
+GROUP BY o2.rnk
