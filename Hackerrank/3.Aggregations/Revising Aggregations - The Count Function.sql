f374dc32a5782e44cf3a986628a092edf0916e2c
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-01-03 03:01:50 +1100

    Create Revising Aggregations - The Count Function.sql

---
+++ Hackerrank/3.Aggregations/Revising Aggregations - The Count Function.sql
@@ -0,0 +1,8 @@
+# Problem Statement: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+# Difficulty: Easy
+# Score: 10
+
+- MySQL query:
+
+SELECT COUNT(NAME) FROM CITY
+WHERE POPULATION > 100000;
