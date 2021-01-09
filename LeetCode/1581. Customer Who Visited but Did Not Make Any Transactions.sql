6cc8e2a558d48b52b214dab1a731bfaddffb2f8f
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-01-10 01:31:20 +1100

    Update 1581. Customer Who Visited but Did Not Make Any Transactions.sql

--- LeetCode/1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++ LeetCode/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -8,3 +8,17 @@
 SELECT customer_id, COUNT(visit_id) as count_no_trans FROM Visits
 WHERE visit_id NOT IN (SELECT DISTINCT visit_id as visit_id_unique from Transactions)
 GROUP BY customer_id;
+
+- MySQL query using JOIN:
+#Reference: verisimilitude, https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/discuss/865760/Simplest-Just-a-LEFT-JOIN-No-SUB-Query.
+
+SELECT
+	customer_id,
+	COUNT(Visits.visit_id) AS count_no_trans
+FROM
+	visits
+LEFT JOIN Transactions
+	ON Visits.visit_id = Transactions.visit_id
+WHERE 
+	Transactions.visit_id IS NULL
+GROUP BY customer_id
