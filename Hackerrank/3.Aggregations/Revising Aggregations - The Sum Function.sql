4c7fa8b587870c91e9d070b307a5dbd89d900dc7
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-01-15 17:09:14 +1100

    Create Revising Aggregations - The Sum Function.sql

---
+++ Hackerrank/3.Aggregations/Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,8 @@
+# Problem Statement: https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+# Difficulty: Easy
+# Score: 10
+
+- MySQL query:
+
+SELECT SUM(POPULATION) FROM CITY
+WHERE DISTRICT = "California"
