ac4d60c7e68baa592ab0b7698aaeb21a4690de87
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-01-20 00:38:30 +1100

    Update 1571. Warehouse Manager.sql

--- LeetCode/1571. Warehouse Manager.sql
+++ LeetCode/1571. Warehouse Manager.sql
@@ -4,8 +4,8 @@
 
 - MySQL query:
 
-SELECT a.name as warehouse_name, SUM(a.units*b.Width*b.Length*b.Height) as volume
-FROM Warehouse as a
-JOIN Products as b
+SELECT a.name AS warehouse_name, SUM(a.units*b.Width*b.Length*b.Height) AS volume
+FROM Warehouse AS a
+JOIN Products AS b
 ON a.product_id = b.product_id
 GROUP BY a.name
