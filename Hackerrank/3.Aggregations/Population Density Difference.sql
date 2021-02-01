4cff499891227b3abd536eeb84e6f4c3b046fa7a
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-02-01 16:33:49 +1100

    Create Population Density Difference.sql

---
+++ Hackerrank/3.Aggregations/Population Density Difference.sql
@@ -0,0 +1,8 @@
+# Problem Statement: https://www.hackerrank.com/challenges/population-density-difference/problem
+# Difficulty: Easy
+# Score: 10
+
+- # MySQL:
+
+SELECT MAX(POPULATION) - MIN(POPULATION)
+FROM CITY
