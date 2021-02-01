04b9b2cffada8f2ccee391e283a9b7f2f571a712
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-02-01 15:22:52 +1100

    Create Japan Population.sql

---
+++ Hackerrank/3.Aggregations/Japan Population.sql
@@ -0,0 +1,9 @@
+# Problem Statement: https://www.hackerrank.com/challenges/japan-population/problem
+# Difficulty: Easy
+# Score: 10
+
+- # MySQL
+
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE COUNTRYCODE = "JPN"
