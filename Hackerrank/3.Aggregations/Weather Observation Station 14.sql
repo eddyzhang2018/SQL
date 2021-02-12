a807a9ef6208c413b2b742db61c4000c0bdf3d95
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-02-12 15:47:28 +1100

    Create Weather Observation Station 14.sql

---
+++ Hackerrank/3.Aggregations/Weather Observation Station 14.sql
@@ -0,0 +1,7 @@
+# Problem Statement: https://www.hackerrank.com/challenges/weather-observation-station-14/problem
+# Difficulty: Easy
+# Score: 10
+
+SELECT TRUNCATE(MAX(LAT_N), 4)
+FROM STATION
+WHERE LAT_N < 137.2345
