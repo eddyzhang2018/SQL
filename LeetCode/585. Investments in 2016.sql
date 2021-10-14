f1a1b9127ea03f1817b9f717f8153ea94671da1f
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-10-15 00:04:45 +1100

    Update 585. Investments in 2016.sql

--- LeetCode/585. Investments in 2016.sql
+++ LeetCode/585. Investments in 2016.sql
@@ -1,1 +1,11 @@
+# Task: Write a query to print the sum of all total investment values in 2016 (TIV_2016), to a scale of 2 decimal places, for all policy holders who meet the following criteria: 
+    # Have the same TIV_2015 value as one or more other policyholders.
+    # Are not located in the same city as any other policyholder (i.e.: the (latitude, longitude) attribute pairs must be unique)
 # Problem Statement: https://leetcode.com/problems/investments-in-2016/
+# Difficulty: Medium
+
+SELECT ROUND(SUM(insurance.TIV_2016),2) AS TIV_2016 FROM insurance
+WHERE PID IN 
+(SELECT PID FROM insurance GROUP BY LAT, LON HAVING COUNT(*) = 1 )
+AND PID NOT IN 
+(SELECT PID FROM insurance GROUP BY TIV_2015 HAVING COUNT(*) = 1 ) 
